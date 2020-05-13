9c2d63318f9559595fabe13be82f6e83bfd86613
Sean Mcleish <sean.mcleish@laytec.de>
2020-05-13 13:50:00 +0200

    Remove javascript interop from example

--- Plotly.Blazor.Examples/wwwroot/plotly-interop.js
+++
@@ -1,8 +0,0 @@
-﻿window.plotlyInterop = {
-    newPlot: function (id, data = [], layout = {}, config = {}) {
-        window.Plotly.newPlot(id, data, layout, config);
-    },
-    react: function (id, data=[], layout={}, config={}) {
-        window.Plotly.react(id, data, layout, config);
-    }
-}

ba18c80d032d213d21422601f69615eb22032084
Matthew R Johnson <matthew@centreboard.net>
2022-02-28 20:20:38 +0000

    Add temporary examples of event handling

---
+++ Plotly.Blazor.Examples/EventHelper.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+using Microsoft.AspNetCore.Components;
+
+namespace Plotly.Blazor.Examples
+{
+
+    public class XYClickEventArgs : EventArgs
+    {
+        public string X { get; set; }
+        public int Y { get; set; }
+    }
+
+    public class PieClickEventArgs : EventArgs
+    {
+        public string Label { get; set; }
+        public int Value { get; set; }
+    }
+    
+    public class PieHoverEventArgs : EventArgs
+    {
+        public string Label { get; set; }
+        public int Value { get; set; }
+    }
+    
+    public class NoDataEventArgs : EventArgs { }
+
+    [EventHandler("onplotly_click_xy", typeof(XYClickEventArgs), enableStopPropagation: true, enablePreventDefault: true)]
+    [EventHandler("onplotly_click_pie", typeof(PieClickEventArgs), enableStopPropagation: true, enablePreventDefault: true)]
+    [EventHandler("onplotly_hover_pie", typeof(PieHoverEventArgs), enableStopPropagation: true, enablePreventDefault: true)]
+    [EventHandler("onplotly_doubleclick", typeof(NoDataEventArgs), enableStopPropagation: true, enablePreventDefault: true)]
+    public static class EventHandlers { }
+}
---
+++ Plotly.Blazor.Examples/wwwroot/plotly-events.js
@@ -0,0 +1,36 @@
+﻿window.plotlyExampleEvents = {
+    plotlyClickXYEventArgsCreator: function (event) {
+        return {
+            x: event.detail.points[0].x,
+            y: event.detail.points[0].y
+        };
+    },
+    plotlyPIEEventArgsCreator: function (event) {
+        return {
+            label: event.detail.points[0].label,
+            value: event.detail.points[0].value
+        };
+    },
+    plotlyNoDataEventArgsCreator: function (event) {
+        // No data associated with event
+        return { };
+    }
+}
+
+// Register custom event names and handlers
+Blazor.registerCustomEventType('plotly_click_pie', {
+    browserEventName: 'plotly_click',
+    createEventArgs: plotlyExampleEvents.plotlyPIEEventArgsCreator
+});
+Blazor.registerCustomEventType('plotly_hover_pie', {
+    browserEventName: 'plotly_hover',
+    createEventArgs: plotlyExampleEvents.plotlyPIEEventArgsCreator
+});
+Blazor.registerCustomEventType('plotly_click_xy', {
+    browserEventName: 'plotly_click',
+    createEventArgs: plotlyExampleEvents.plotlyClickXYEventArgsCreator
+});
+Blazor.registerCustomEventType('plotly_doubleclick', {
+    browserEventName: 'plotly_doubleclick',
+    createEventArgs: plotlyExampleEvents.plotlyNoDataEventArgsCreator
+});

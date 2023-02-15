078e995c826b44c04907256eee191d68f3600b99
Ishaan Pratap <ishaan.pratap@3cssoftware.com.au>
2023-02-16 09:32:29 +1000

    add relayout event interop

---
+++ Plotly.Blazor/Interop/RelayoutEventData.cs
@@ -0,0 +1,33 @@
+﻿using Plotly.Blazor.LayoutLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Plotly.Blazor.Interop
+{
+
+    /// <summary>
+    ///     This class is used to parse the event data from the relayout jsinterop action.
+    /// </summary>
+    public class RelayoutEventData
+    {
+        /// <summary>
+        ///     The x-axis of the layout. [x0, x1]
+        /// </summary>
+        /// <remarks>
+        ///     In some cases this may be not be set.
+        /// </remarks>
+        public double[] XRange { get; set; }
+
+
+        /// <summary>
+        ///     The y-axis of the layout. [y0, y1].
+        /// </summary>
+        /// <remarks>
+        ///      In some cases this may be not be set.
+        /// </remarks>
+        public double[] YRange { get; set; }
+    }
+}

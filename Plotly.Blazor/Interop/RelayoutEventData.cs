a87001bf70d27cf005e16fd6d1ca463ff395724b
Sean McLeish <sean.mcLeish@laytec.de>
2024-06-17 15:13:03 +0200

    Add z-axis to relayout event and update generator files

--- Plotly.Blazor/Interop/RelayoutEventData.cs
+++ Plotly.Blazor/Interop/RelayoutEventData.cs
@@ -21,5 +21,13 @@
         ///      In some cases this may be not be set.
         /// </remarks>
         public object[] YRange { get; set; }
+
+        /// <summary>
+        ///     The z-axis of the layout. [z0, z1].
+        /// </summary>
+        /// <remarks>
+        ///      In some cases this may be not be set.
+        /// </remarks>
+        public object[] ZRange { get; set; }
     }
 }
